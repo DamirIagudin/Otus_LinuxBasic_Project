ee3758d8b5e14fbadba4dfec49376d2f7e5dbf8a
Damir <108293716+DamirIagudin@users.noreply.github.com>
2023-07-21 21:03:33 +0200

    Create grafana.sh

---
+++ grafana.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# установка Grafana OSS
+apt-get install -y apt-transport-https
+apt-get install -y software-properties-common wget
+wget -q -O /usr/share/keyrings/grafana.key https://apt.grafana.com/gpg.key
+echo "deb [signed-by=/usr/share/keyrings/grafana.key] https://apt.grafana.com stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+apt-get update
+apt-get install grafana
+damir@mysql-replica:~/Otus_LinuxBasic_Project$
