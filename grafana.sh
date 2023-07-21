e82f566a07875300aa4fb59e239192976adde7f7
Damir <108293716+DamirIagudin@users.noreply.github.com>
2023-07-21 21:09:56 +0200

    Update grafana.sh

--- grafana.sh
+++ grafana.sh
@@ -7,4 +7,3 @@ wget -q -O /usr/share/keyrings/grafana.key https://apt.grafana.com/gpg.key
 echo "deb [signed-by=/usr/share/keyrings/grafana.key] https://apt.grafana.com stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
 apt-get update
 apt-get install grafana
-damir@mysql-replica:~/Otus_LinuxBasic_Project$
