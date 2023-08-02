fea0c39fe500636d0f4cf14e22dd35072ff7d616
Damir <108293716+DamirIagudin@users.noreply.github.com>
2023-08-02 22:39:05 +0200

    Update apache.sh

--- apache.sh
+++ apache.sh
@@ -4,19 +4,19 @@
 mkdir /var/www/{8080..8082}
 
 # Копируем уже готовые страницы сайтов с разным содежимым для apache 
-cp /home/damir/Otus_LinuxBasic_Project/html8080.html /var/www/8080/index.html
-cp /home/damir/Otus_LinuxBasic_Project/html8081.html /var/www/8081/index.html
-cp /home/damir/Otus_LinuxBasic_Project/html8082.html /var/www/8082/index.html
+cp html8080.html /var/www/8080/index.html
+cp html8081.html /var/www/8081/index.html
+cp html8082.html /var/www/8082/index.html
 
 # Копируем уже готовые файла конфигурации виртуального хостинга (для обращения к каждому порту) в директорию /etc/apache2/sites-available/
-cp /home/damir/Otus_LinuxBasic_Project/{8080..8082}.conf /etc/apache2/sites-available
+cp {8080..8082}.conf /etc/apache2/sites-available
 
 # Включем конфиги в конфигурацию, по сути создаем символические ссылки. 
 #(Этим процедура настройки для Debian и Ubuntu отличается от настройки для CentOS)
 a2ensite {8080..8082}.conf
 
 # Подменяем (копируем) /etc/apache2/ports.conf в котором прописаны номера портов для обращения Apache. 
-cp /home/damir/Otus_LinuxBasic_Project/ports.conf /etc/apache2/ports.conf
+cp ports.conf /etc/apache2/ports.conf
 
 # Выполняем reload процесса apache2, чтобы применить новую конфигурацию:
 systemctl reload apache2
