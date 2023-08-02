61de5c90b6e4c1ac85d00aac38c43655f8232a34
Damir <108293716+DamirIagudin@users.noreply.github.com>
2023-08-02 22:40:13 +0200

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # В директорию /etc/nginx/sites-available/ копирует готовый файл конфигурации balance-server.conf.
-cp /home/damir/Otus_LinuxBasic_Project/balance-server.conf /etc/nginx/sites-available/
+cp balance-server.conf /etc/nginx/sites-available/
 
 # Чтобы включить файл balance-server.conf в конфигурацию создаем на него символьную ссылку (SoftLink) в папке /etc/nginx/sites-enabled/
 ln -s /etc/nginx/sites-available/balance-server.conf /etc/nginx/sites-enabled/balance-server.conf
