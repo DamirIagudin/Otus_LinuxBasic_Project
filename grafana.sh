e382f6bfdfc4fc5ad0a87365507ac7fc2afd1128
Damir <108293716+DamirIagudin@users.noreply.github.com>
2023-08-02 22:27:31 +0200

    Update grafana.sh

--- grafana.sh
+++ grafana.sh
@@ -7,3 +7,6 @@ wget -q -O /usr/share/keyrings/grafana.key https://apt.grafana.com/gpg.key
 echo "deb [signed-by=/usr/share/keyrings/grafana.key] https://apt.grafana.com stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
 apt-get update
 apt-get install grafana
+
+#запускаем, не ставим в автозагрузку 
+sudo systemctl start grafana-server.service
