d6a4ab18e15b932033249f861db572c74fe3d7f4
Damir <108293716+DamirIagudin@users.noreply.github.com>
2023-08-06 16:32:47 +0200

    Update mysql_source.sh

--- mysql_source.sh
+++ mysql_source.sh
@@ -1,8 +1,9 @@
+#!/bin/bash
+
 #Установка пакета из стандартного репозитория
 apt install mysql-server
 
 #По умолчанию было настроено только чтобы слушать localhost.
 #должен слушать не только localhost, но и другие адреса! 
 #Делаем изменение в конфиг файле: /etc/mysql/mysql.conf.d/mysqld.cnf
-#Изменить текстовый файл и сохранить. mysqlx-bind-address - шаблон для поиска заданной строки
 sed -i '/mysqlx-bind-address/s/127.0.0.1/0.0.0.0/' /etc/mysql/mysql.conf.d/mysqld.cnf
