e5e7ee2dac3ca28464253a57ce29e4d863401c8f
Damir <108293716+DamirIagudin@users.noreply.github.com>
2023-08-06 16:32:14 +0200

    Create mysql_replica.sh

---
+++ mysql_replica.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Установка пакета из стандартного репозитория
+apt install mysql-server
+
+#Меняем настройки в конфиге, должен слушать не только localhost, но и другие адреса
+sudo sed -i '/mysqlx-bind-address/s/127.0.0.1/0.0.0.0/' /etc/mysql/mysql.conf.d/mysqld.cnf
+#Меняем сервер ID, чтобы отличались у мастера и реплики.
+sudo sed -i '/# server-id/c server-id  = 2' /etc/mysql/mysql.conf.d/mysqld.cnf
+
+#после чего делаем рестарт:
+systemctl restart mysql
