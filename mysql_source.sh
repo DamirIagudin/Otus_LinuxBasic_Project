45c8b0883c8bdeb8ddd9174e076edbba72b8a2e3
Damir <108293716+DamirIagudin@users.noreply.github.com>
2023-08-07 22:46:27 +0200

    Update mysql_source.sh

--- mysql_source.sh
+++ mysql_source.sh
@@ -7,3 +7,5 @@ apt install mysql-server
 #должен слушать не только localhost, но и другие адреса! 
 #Делаем изменение в конфиг файле: /etc/mysql/mysql.conf.d/mysqld.cnf
 sed -i '/bind-address/s/127.0.0.1/0.0.0.0/' /etc/mysql/mysql.conf.d/mysqld.cnf
+#восстанавливаем строку mysqlx-bind-address после предыдущей команды 
+sed -i '/mysqlx-bind-address/s/0.0.0.0/127.0.0.1/' /etc/mysql/mysql.conf.d/mysqld.cnf
