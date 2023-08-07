8c124708ee5ca4f90a66311bcbed01d527da5268
Damir <108293716+DamirIagudin@users.noreply.github.com>
2023-08-07 22:46:54 +0200

    Update mysql_replica.sh

--- mysql_replica.sh
+++ mysql_replica.sh
@@ -5,6 +5,8 @@ apt install mysql-server
 
 #Меняем настройки в конфиге, должен слушать не только localhost, но и другие адреса
 sudo sed -i '/bind-address/s/127.0.0.1/0.0.0.0/' /etc/mysql/mysql.conf.d/mysqld.cnf
+#восстанавливаем строку mysqlx-bind-address после предыдущей команды 
+sed -i '/mysqlx-bind-address/s/0.0.0.0/127.0.0.1/' /etc/mysql/mysql.conf.d/mysqld.cnf
 #Меняем сервер ID, чтобы отличались у мастера и реплики.
 sudo sed -i '/# server-id/c server-id  = 2' /etc/mysql/mysql.conf.d/mysqld.cnf
 
